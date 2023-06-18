5dc7ae9085537a47bce97d95fc84db8685b62450
Boamah-Powers <kb4242@nyu.edu>
2023-06-18 22:07:43 +0400

    Updates

---
+++ Portfolio/contactform/contact_form.js
@@ -0,0 +1,17 @@
+function sendEmail() {
+	Email.send({
+		SecureToken: "93df7bbb-ab10-49ef-b04b-2020fc12fb21",
+	    // Host : "smtp.elasticemail.com",
+	    // Username : "kb4242@nyu.edu",
+	    // Password : "fyowfhytmvwbstje",
+	    To : 'kwaabp24@gmail.com',
+	    From : document.getElementById("email").value,
+	    Subject : "New Contact Form Enquiry",
+	    Body : "Name: " + document.getElementById("name").value
+	    	+ "<br> Email: " + document.getElementById("email").value
+	    	+ "<br> Phone: " + document.getElementById("phone").value
+	    	+ "<br> Message: " + document.getElementById("message").value
+	}).then(
+	  message => alert("Message sent successfully")
+	);
+}
